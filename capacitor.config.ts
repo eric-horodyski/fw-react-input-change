1805d0d5d0cdbcb25c944666e6b20f76cf899284
Eric Horodyski <horodyski@ionic.io>
2021-06-02 15:54:33 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fw-react-input-change',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
